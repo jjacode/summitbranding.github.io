6eb021aeb4b4cf27e2e606e4cbf7eeda120d4ad3
Jason J. Anderson <jasonjanderson@outlook.com>
2018-06-14 03:38:37 -0600

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className='App'>
+				<h1>Summit Branding</h1>
+				<p>Under construction, please come back soon. We're changing a ton of our stuff.</p>
+			</div>
+		);
+	}
+}
